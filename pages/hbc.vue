4fb81a789071481e90d92663b9fedb84b7df6dd9
pythonthm <pythonthm@gmail.com>
2023-06-14 15:45:28 +0900

    HBC

--- pages/hbc.vue
+++ pages/hbc.vue
@@ -1,7 +1,44 @@
 <template>
-    <div>
-        <h1>
-            This is HBC
-        </h1>
-    </div>
+  <h1 style="color: #c71585">北海道文化放送</h1>
+
+  <div class="img-container">
+    <img style="width: 70%" src="~/assets/hbc/hbc_01.jpg" />
+  </div>
+
+  <div class="access-guide">
+    <ul>
+      <li>
+        <span>見学 & 報道勉強</span> <br />
+        12:30～14:00（1時間30分）
+      </li>
+    </ul>
+  </div>
+  <h3>アクセス</h3>
+  <div class="access-container">
+    <img class="access" style="width: 75%" src="~/assets/hbc/hbc_02.jpg" />
+  </div>
+  <br />
+  <!-- <h3>館内施設 紹介</h3> -->
+  <img class="access" style="width: 70%" src="~/assets/hbc/hbc_03.jpg" />
+  <br />
+  <br />
+  <br />
 </template>
+
+<style scoped>
+  .access-guide {
+    text-align: center;
+  }
+  span {
+    color: red;
+  }
+  .access {
+    text-align: center;
+    margin-left: 0.5rem;
+    margin-right: 0.5rem;
+  }
+
+  #left {
+    margin-left: 1.2rem;
+  }
+</style>
