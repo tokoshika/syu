4c9e3086bcb72a161be4c6e685f4f4a8cf9d8990
pythonthm <pythonthm@gmail.com>
2023-06-14 16:06:32 +0900

    白い恋人パーク

--- pages/shiroikoibito.vue
+++ pages/shiroikoibito.vue
@@ -1,7 +1,62 @@
 <template>
-    <div>
-        <h1>
-            This is shiroikoibito
-        </h1>
-    </div>
+  <h1 style="color: maroon">白い恋人パーク</h1>
+
+  <div class="img-container">
+    <img style="width: 60%" src="~/assets/shiroi/shiroi_01.jpg" />
+  </div>
+  <h3>アクセス</h3>
+
+  <div class="access-guide">
+    <ul>
+      <!-- <li>
+        <span>交通機関利用</span> <br />
+        地下鉄東西線「円山公園」駅下車 → 2番出口より円山バスターミナルへ<br />
+        路線バス「大倉山線くらまる号」乗車 およそ15分 → 「大倉山ジャンプ競技場」
+      </li> -->
+
+      <!-- <li>
+          <span>入館料</span> <br />
+          入館は無料です
+        </li> -->
+    </ul>
+  </div>
+
+  <div class="access-container">
+    <img
+      class="access"
+      style="width: 75%"
+      src="~/assets/shiroi/shiroi_02.jpg"
+    />
+  </div>
+  <br />
+  <h3>館内施設 紹介</h3>
+  <img class="access" style="width: 70%" src="~/assets/shiroi/shiroi_04.jpg" />
+  <br />
+
+  <br />
+  <img class="access" style="width: 70%" src="~/assets/shiroi/shiroi_05.jpg" />
+  <br />
+
+  <a href="https://www.shiroikoibitopark.jp/">白い恋人パーク</a>
+  <br />
+  <br />
+  <br />
 </template>
+
+<style scoped>
+  .access-guide {
+    text-align: center;
+  }
+  span {
+    color: red;
+  }
+  .access {
+    text-align: center;
+    margin-left: 0.5rem;
+    margin-right: 0.5rem;
+  }
+
+  #left {
+    margin-left: 1.2rem;
+  }
+</style>
