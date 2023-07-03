fdec273a5be5da047b600d51a36f3c9f6c54d032
pythonthm <pythonthm@gmail.com>
2023-07-04 08:24:32 +0900

    99

--- pages/test.vue
+++ pages/test.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
+    <h1 style="color: rgb(220, 132, 116)">札幌地下鉄　路線図</h1>
+    <h2>地図をタップして料金を調べよう</h2>
     <a href="https://www.city.sapporo.jp/st/ryokin/index.html">
-      <img src="assets\subway.png" style="width: 100%" alt="" srcset="" />
+      <img src="assets\subway.jpg" style="width: 70%" alt="" srcset="" />
     </a>
   </div>
 </template>
+<style>
+  h1 {
+    margin-top: 1rem;
+  }
+
+  h2 {
+    margin: 0;
+  }
+</style>
